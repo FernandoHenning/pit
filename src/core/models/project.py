fd09b9c6f9657a6fc9ddb897e3dedccf556e4e6e
FernandoHenning <fernando_hema@hotmail.com>
2024-01-04 19:01:03 -0600

    fix(project_model): description and repository must be optional

--- src/core/models/project.py
+++ src/core/models/project.py
@@ -5,8 +5,8 @@ from core.constants import DEFAULT_PROJECT_IMAGE
 
 class Project(models.Model):
     name = models.CharField(max_length=100, unique=True, blank=False, null=False)
-    description = models.TextField(blank=False, null=False)
-    repository = models.URLField(blank=False, null=False)
+    description = models.TextField(blank=True)
+    repository = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     image_link = models.URLField(blank=False, null=False, default=DEFAULT_PROJECT_IMAGE)
 
